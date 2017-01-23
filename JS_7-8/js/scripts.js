50c6634e0b52e4f9ff48b1ce88869ad46c418503
Olesya Kogivchak <lesya.klochko@gmail.com>
2017-01-23 19:09:06 +0200

    fixed TAB

--- JS_7-8/js/scripts.js
+++ JS_7-8/js/scripts.js
@@ -8,15 +8,15 @@ var $text1 = $('.main__text_1');
 var $text2 = $('.main__text_2');
 var $text3 = $('.main__text_3');
 
+
+
 $link1.on('click', function(e) {
   e.preventDefault();
   $link1.css({background: '#33CCCC'});
   $link2.css({background: '#999'});
   $link3.css({background: '#999'});
-  $text1.css({display: 'inherit'});
-  $text2.css({display: 'none'});
-  $text3.css({display: 'none'});
-
+  $('p').removeClass('main__text_active');
+  $text1.addClass("main__text_active");
 
 });
 
@@ -25,12 +25,8 @@ $link2.on('click', function(e) {
   $link2.css({background: '#33CCCC'});
   $link1.css({background: '#999'});
   $link3.css({background: '#999'});
-  $text2.css({display: 'inherit'});
-  $text1.css({display: 'none'});
-  $text3.css({display: 'none'});
-
-
-
+  $('p').removeClass('main__text_active');
+  $text2.addClass("main__text_active");
 
 });
 
@@ -39,9 +35,8 @@ $link3.on('click', function(e) {
   $link3.css({background: '#33CCCC'});
   $link1.css({background: '#999'});
   $link2.css({background: '#999'});
-  $text3.css({display: 'inherit'});
-  $text1.css({display: 'none'});
-  $text2.css({display: 'none'});
+  $('p').removeClass('main__text_active');
+  $text3.addClass("main__text_active");
 
 });
 
