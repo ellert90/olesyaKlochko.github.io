8187fecc58701ecac07e5c6ab21a6162cf8c56ab
Olesya Kogivchak <lesya.klochko@gmail.com>
2017-01-23 19:28:51 +0200

    fixed

--- JS_7-8/js/scripts.js
+++ JS_7-8/js/scripts.js
@@ -3,7 +3,6 @@ $(function () {
 var $link1 = $('.main__item_1');
 var $link2 = $('.main__item_2');
 var $link3 = $('.main__item_3');
-
 var $text1 = $('.main__text_1');
 var $text2 = $('.main__text_2');
 var $text3 = $('.main__text_3');
@@ -12,9 +11,8 @@ var $text3 = $('.main__text_3');
 
 $link1.on('click', function(e) {
   e.preventDefault();
-  $link1.css({background: '#33CCCC'});
-  $link2.css({background: '#999'});
-  $link3.css({background: '#999'});
+  $('li').removeClass('main__item_active');
+  $(this).addClass("main__item_active");
   $('p').removeClass('main__text_active');
   $text1.addClass("main__text_active");
 
@@ -22,9 +20,8 @@ $link1.on('click', function(e) {
 
 $link2.on('click', function(e) {
   e.preventDefault();
-  $link2.css({background: '#33CCCC'});
-  $link1.css({background: '#999'});
-  $link3.css({background: '#999'});
+  $('li').removeClass('main__item_active');
+  $(this).addClass("main__item_active");
   $('p').removeClass('main__text_active');
   $text2.addClass("main__text_active");
 
@@ -32,9 +29,8 @@ $link2.on('click', function(e) {
 
 $link3.on('click', function(e) {
   e.preventDefault();
-  $link3.css({background: '#33CCCC'});
-  $link1.css({background: '#999'});
-  $link2.css({background: '#999'});
+  $('li').removeClass('main__item_active');
+  $(this).addClass("main__item_active");
   $('p').removeClass('main__text_active');
   $text3.addClass("main__text_active");
 
