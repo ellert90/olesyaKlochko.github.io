927b784fb7de31c4310f3692c49894c5101e63d8
Olesya Kogivchak <lesya.klochko@gmail.com>
2017-01-25 17:39:34 +0200

    JS_7-8_Form

---
+++ JS_7-8_Form/js/scripts.js
@@ -0,0 +1,19 @@
+
+$(function () {
+
+  var span = $('span');
+  span.hide();
+  
+$('.form__input').hover(function(e) {
+   e.preventDefault();
+   span.hide();
+   $(this).next('span').show();
+});
+
+$(".form__button").on("click", function(e) {
+  e.preventDefault();
+  span.show();
+});
+
+
+});
