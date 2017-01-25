2b732ce9eebc1223d3ffd4ede7a5af301988d290
Olesya Kogivchak <lesya.klochko@gmail.com>
2017-01-25 17:44:01 +0200

    fixed focus

--- JS_7-8_Form/js/scripts.js
+++ JS_7-8_Form/js/scripts.js
@@ -3,8 +3,14 @@ $(function () {
 
   var span = $('span');
   span.hide();
-  
+
 $('.form__input').hover(function(e) {
+   e.preventDefault();
+   span.hide();
+   $(this).next('span').show();
+});
+
+$('.form__input').focus(function(e) {
    e.preventDefault();
    span.hide();
    $(this).next('span').show();
