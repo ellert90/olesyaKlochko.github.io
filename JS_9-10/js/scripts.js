444cfe850725f3e5692efb9e3e12f06484b48e35
Olesya Kogivchak <lesya.klochko@gmail.com>
2017-01-29 23:06:06 +0200

    js 9-10

---
+++ JS_9-10/js/scripts.js
@@ -0,0 +1,39 @@
+$(function() {
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+
+      $('.basic').fancySelect();
+
+      var $link = $('#sub');
+      $link.hover(
+            function(){
+                $(this).children('.submenu').slideDown(500);
+            },
+            function(){
+                $(this).children('.submenu').slideUp(500);
+            });
+
+
+    });
