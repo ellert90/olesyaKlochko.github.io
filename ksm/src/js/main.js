db50eb40067dd77e8bb15dd81fb079fafb6e65e9
Olesya Kogivchak <lesya.klochko@gmail.com>
2017-04-22 15:53:04 +0300

    order

--- ksm/src/js/main.js
+++ ksm/src/js/main.js
@@ -76,6 +76,28 @@ $(function () {
 
   });
 
+// ставимо колбек на клік мишкою по кнопці перевірити результат
+let btn = document.getElementById('order__button');
+btn.addEventListener("click", function(event) {
+showResults();// показуємо результат
+event.preventDefault(); // так як у нас ставиться форма, зробимо щоб сторінка не перегружалася
+});
+
+// реалізація модального вікна
+let btnTestClose = document.getElementsByClassName('order-window__click')[0];
+let resultsWindow = document.getElementsByClassName('order-window')[0];
+    btnTestClose.addEventListener("click", function(event) {
+    resultsWindow.setAttribute('style', 'display: none;');
+    event.preventDefault();
+});
+
+
+// функція, яка показує вікно для відправки замовлення
+function showResults() {
+resultsWindow.setAttribute('style', 'display: block;');
+
+}
+
 $(function() {
 var htmlServices = $('#dataServices').html();
 var dataServices = [
