4f974d2096edfeb270b3266240d0a16e9416f44d
Olesya Kogivchak <lesya.klochko@gmail.com>
2017-04-26 09:51:45 +0300

    ft added

---
+++ tourFactory/src/blocks/js/services.js
@@ -0,0 +1,187 @@
+$(function() {
+var htmlCountries = $('#dataCountries').html();
+var dataCountries = [
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+},
+{
+  countryTitle: 'Турция',
+  img: 'img/tr.png',
+  link: '#'
+}
+];
+
+var content = tmpl(htmlCountries, {
+  data: dataCountries
+});
+
+$('#countries').append(content);
+
+});
